80ffd1e2ac56fc1ae53bf7abd094164c6b528da3
apanariello4 <nellopanariello96@gmail.com>
2023-03-14 13:57:10 +0100

    add fish

---
+++ goten/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/nello/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# conda activate mammoth
+# <<< conda initialize <<<
+
